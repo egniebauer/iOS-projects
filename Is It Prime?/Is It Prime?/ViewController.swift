d5a78fdacc2a3761e1614573334375280804988d
Elizabeth G. Niebauer <elizabethniebauer@icloud.com>
2017-04-03 14:39:55 -0500

    update output to include user input

--- Is It Prime?/Is It Prime?/ViewController.swift
+++ Is It Prime?/Is It Prime?/ViewController.swift
@@ -10,55 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var userInput: UITextField?
+    @IBOutlet weak var userInput: UITextField!
     @IBOutlet weak var responseLabel: UILabel!
     
     @IBAction func submitPressed(_ sender: Any) {
         
-        // verify input
-        if verifyInput(possibleInt: userInput!) {
+        let inputString = userInput.text!
+        
+        if verifyInput(possibleInt: inputString) {
             
-            if isPrime(possiblePrime: userInput!) {
-                self.responseLabel.text = "Prime!"
+            let inputNumber = Int(inputString)!
+            
+            if isPrime(possiblePrime: inputNumber) {
+                self.responseLabel.text = "\(inputNumber) is Prime!"
             } else {
-                self.responseLabel.text = "Not Prime!"
+                self.responseLabel.text = "\(inputNumber) is Not Prime!"
             }
-            
         } else {
-            print("User Input: invalid")
-            self.responseLabel.text = "Please enter a whole number (e.g. 1 or 314)"
+            self.responseLabel.text = "Please enter a positive whole number (e.g. 1)"
         }
-        
-        
     }
     
-    func verifyInput (possibleInt: UITextField) -> Bool {
+    func verifyInput (possibleInt: String) -> Bool {
         
-        if Int(possibleInt.text!) != nil {
+        if Int(possibleInt) != nil {
             return true
         } else {
             return false
         }
-        
     }
     
-    func isPrime (possiblePrime: UITextField) -> Bool {
+    func isPrime (possiblePrime: Int) -> Bool {
         
-        if Int(possiblePrime.text!) == 1 {
+        if possiblePrime == 1 {
             return false
-        } else if Int(possiblePrime.text!) == 2 {
+        } else if possiblePrime == 2 {
             return true
-        } else if Int(possiblePrime.text!)! > 2 && (Int(possiblePrime.text!)! % 2 == 0) {
+        } else if possiblePrime > 2 && (possiblePrime % 2 == 0) {
             return false
         } else {
-            for index in 3...(Int(possiblePrime.text!)! - 1) {
-                if Int(possiblePrime.text!)! % index == 0 {
+            for index in 2...(possiblePrime - 1) {
+                if possiblePrime % index == 0 {
                     return false
                 }
             }
         }
         return true
-        
     }
     
     override func viewDidLoad() {
